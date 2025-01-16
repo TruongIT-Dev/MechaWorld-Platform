e1f2b26b81b23c5095a9c885e09db37b2cbdd259
truong-nguyen <dangtruongnguyen2@gmail.com>
2025-01-16 13:51:18 +0700

    Updated Loading page

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,12 +1,16 @@
+import { useEffect } from "react";
+
 export default function Spinner() {
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            const spinner = document.getElementById("spinner");
+            if (spinner) {
+                spinner.classList.remove("show");
             }
-        }, 10);
-    };
-    spinner();
+        }, 500); // Ẩn spinner sau 500ms
+        return () => clearTimeout(timer); // Dọn dẹp timer
+    }, []);
+
     return (
         <>
             <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
@@ -15,5 +19,5 @@ export default function Spinner() {
                 </div>
             </div>
         </>
-    )
+    );
 }
