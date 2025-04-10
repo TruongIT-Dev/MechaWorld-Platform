f6014a4722954e2f4d56568e82fc26bc56b55125
huyftb <manhhuyftb@gmail.com>
2025-04-10 18:05:00 +0700

    check 2

--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -263,7 +263,7 @@ const ProfilePage = () => {
               {/* Số điện thoại */}
               <Form.Item label="Số điện thoại">
                 <div className="flex items-center">
-                  <Input value={newPhoneNumber || "Chưa đăng kí"} disabled className="bg-gray-100" />
+                  <Input value={maskPhoneNumber(newPhoneNumber) || "Chưa đăng kí"} disabled className="bg-gray-100" />
                   <Button type="link" className="ml-2 text-blue-500" onClick={() => setPhoneModalVisible(true)}>{newPhoneNumber ? "Thay đổi" : "Đăng ký"}</Button>
                 </div>
               </Form.Item>
