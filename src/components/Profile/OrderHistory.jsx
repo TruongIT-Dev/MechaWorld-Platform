4427030e4594e134c3b2d1651f8166b564210c78
truong-nguyen <dangtruongnguyen2@gmail.com>
2025-04-13 10:33:51 +0700

    miner update

--- src/components/Profile/OrderHistory.jsx
+++ src/components/Profile/OrderHistory.jsx
@@ -35,9 +35,10 @@ const OrderHistory = () => {
     try {
       const response = await GetListOrderHistory();
       const rawOrders = response.data;
-      
+
       // Lấy danh sách seller_id duy nhất
-      const sellerIds = [...new Set(rawOrders.map(o => o.order.seller_id))];
+      const sellerIds = [...new Set(rawOrders.map(o => o.seller_id))];
+      console.log(sellerIds);
       // Gọi song song API lấy thông tin shop
       const sellerInfoMap = {};
       await Promise.all(sellerIds.map(async (id) => {
@@ -49,27 +50,26 @@ const OrderHistory = () => {
 
       // Gom đơn theo shop
       const grouped = rawOrders.reduce((acc, order) => {
-        const sellerId = order.order.seller_id;
+        const sellerId = order.seller_id;
         if (!acc[sellerId]) {
           const shopInfo = sellerInfoMap[sellerId];
-          console.log(order);
           acc[sellerId] = {
             shopName: shopInfo.full_name,
             shopIcon: shopInfo.avatar_url,
-            status: convertStatus(order.order.status),
-            statusColor: getStatusColor(order.order.status),
+            status: convertStatus(order.status),
+            statusColor: getStatusColor(order.status),
             orders: [],
-            totalPrice: formatCurrency(order.order.total_amount),
+            totalPrice: formatCurrency(order.total_amount),
           };
         }
 
         acc[sellerId].orders.push({
           id: order.id,
-          productName: `Đơn hàng #${order.order.code}`,
-          image: order.order.packaging_images[0] || "https://source.unsplash.com/100x100/?product",
+          productName: `Đơn hàng #${order.code}`,
+          image: "https://source.unsplash.com/100x100/?product",
           // image: "https://source.unsplash.com/100x100/?package",
           quantity: 1,
-          price: formatCurrency(order.order.total_amount),
+          price: formatCurrency(order.total_amount),
         });
 
         return acc;
